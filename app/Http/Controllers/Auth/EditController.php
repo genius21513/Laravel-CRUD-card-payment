0b753edcf951f6415f089498e2189ad2eda244e2
genius21513 <genius21513@gmail.com>
2022-10-30 20:33:41 +0900

    Add accounts tab

--- app/Http/Controllers/Auth/EditController.php
+++ app/Http/Controllers/Auth/EditController.php
@@ -11,4 +11,17 @@ class EditController extends Controller
     {
         return view('auth.edit');
     }
+
+    public function update()
+    {
+        // $request->validate([
+        //     'name' =>'required|min:4|string|max:255',
+        //     'email'=>'required|email|string|max:255'
+        // ]);
+        // $user =Auth::user();
+        // $user->name = $request['name'];
+        // $user->email = $request['email'];
+        // $user->save();
+        // return back()->with('message','Profile Updated');
+    }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('users.index') }}">{{ __('Users') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('dashboard') }}">{{ __('Accounts') }}</a>
+                        </li>
                         @endauth
                     </ul>
 
